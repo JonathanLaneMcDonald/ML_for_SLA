d17d1e63d588236cf085f332e0047d8aef67098c
Lane McDonald <Jonathan.Lane.McDonald@gmail.com>
2020-10-12 22:18:06 +1100

    formatting and variable name changes in comment selection

--- src/Stage 1 - Comment Selection.py
+++ src/Stage 1 - Comment Selection.py
@@ -1,4 +1,3 @@
-
 from hashlib import sha256
 
 # comments containing any of these characters will be excluded from the dataset
@@ -8,29 +7,28 @@ exclude = {x for x in list(':&')}
 written = set()
 
 # write comments to files so we can review kept/discarded or train on kept comments
-clean = open('japanese comments','w',encoding='utf-8')
-dirty = open('japanese comments - dirty','w',encoding='utf-8')
+clean = open('japanese comments', 'w', encoding='utf-8')
+dirty = open('japanese comments - dirty', 'w', encoding='utf-8')
 
 # comments were saved in the following format
 # <0000337.html 005>「県内」といえばやっぱし県内なので、「都内」も都内なのではないでしょうか。
 # so we know the comment starts after the first '>'
-with open('japanese comments [raw]','r',encoding='utf-8') as infile:
+with open('japanese comments [raw]', 'r', encoding='utf-8') as infile:
 	for line in infile:
-		start = line.find('>')+1
+		start = line.find('>') + 1
 		this_line = line[start:]
 
 		# if a comment is 'clean' and hasn't been written, write it and save the hash
 		if set(list(this_line)) - exclude == set(list(this_line)):
-			hash = sha256(bytes(this_line,encoding='utf-8')).digest()
-			if hash in written:
+			comment_hash = sha256(bytes(this_line, encoding='utf-8')).digest()
+			if comment_hash in written:
 				pass
 			else:
 				clean.write(this_line)
-				written.add(hash)
+				written.add(comment_hash)
 		# we just want to be able to verify that we're discarding the right stuff
 		else:
 			dirty.write(this_line)
 
 clean.close()
 dirty.close()
-		
