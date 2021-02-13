9921809f1a9e5432978383daf56ef0cf907a40c1
Lane McDonald <Jonathan.Lane.McDonald@gmail.com>
2021-02-13 21:47:35 +1100

    cleaned up some whitespace and added a bpe encoder to the sequence tagger

--- src/concrete/SequenceTagger.py
+++ src/concrete/SequenceTagger.py
@@ -1,20 +1,18 @@
-"""
+'''
 the main purpose is to tokenize and label documents using a dictionary of terms
-"""
+'''
 
 import numpy
-
 
 class TreeNode:
 	def __init__(self):
 		self.children = dict()
 		self.isTerminal = False
 
-
 class Tree:
 	"""Represents sequences in a tree for fast string matching"""
 
-	def __init__(self, arrayOfTokenized=[]):
+	def __init__(self, arrayOfTokenized = []):
 		self.root = TreeNode()
 		self.longest_key = 0
 
@@ -51,7 +49,6 @@ class Tree:
 					longest_match = t+1
 		return longest_match
 
-
 class SequenceTagger:
 	"""Provide higher level functionality by wrapping the Tree (Trie) datastructure"""
 
@@ -64,19 +61,19 @@ class SequenceTagger:
 	def getMaxKeyLength(self):
 		return self.tree.getMaxKeyLength()
 
-	def tagDocument(self, tokenizedDoc, boolean_result=False, return_labels=False):
+	def tagDocument(self, tokenizedDoc, boolean_result = False, return_labels = False):
 		"""Receive a sequence and use the built Tree to label subsequences"""
 
-		labels = numpy.zeros(len(tokenizedDoc), dtype=numpy.int)
+		labels = numpy.zeros(len(tokenizedDoc),dtype=numpy.int)
 
-		label_value = 1  # so we can resolve adjacent entities, should they occur adjacently
+		label_value = 1 # so we can resolve adjacent entities, should they occur adjacently
 
 		p = 0
 		while p < len(tokenizedDoc):
 			length = min(self.tree.getMaxKeyLength(), len(tokenizedDoc)-p)
 			matchLength = self.tree.getLongestMatch(tokenizedDoc[p:p+length])
 			if matchLength:
-				for i in range(p, p+matchLength):
+				for i in range(p,p+matchLength):
 					labels[i] = label_value
 				label_value += 1
 				p += matchLength
@@ -89,3 +86,22 @@ class SequenceTagger:
 			return list(zip(tokenizedDoc, [x != 0 for x in labels]))
 		else:
 			return list(zip(tokenizedDoc, labels))
+
+	def bpeEncode(self, token):
+		"""Receive a token and attempt to encode/subdivide it using BPE"""
+
+		bpe_tokens = []
+
+		remnant = token
+		while len(remnant):
+			matchLength = self.tree.getLongestMatch(remnant)
+			if (matchLength > 0 and len(bpe_tokens) == 0) or (matchLength >= 3 and len(bpe_tokens) > 0):
+				bpe_tokens.append(remnant[:matchLength])
+				if matchLength == len(remnant):
+					remnant = ''
+				else:
+					remnant = '##' + remnant[matchLength:]
+			else:
+				return ['[UNK]']
+
+		return bpe_tokens
