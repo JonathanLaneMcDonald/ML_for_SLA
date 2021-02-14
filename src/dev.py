8565f059679154f9bdd6031880745411a4799d1f
Lane McDonald <Jonathan.Lane.McDonald@gmail.com>
2021-02-14 17:20:53 +1100

    update pipeline driver to run on a test size dataset

--- src/dev.py
+++ src/dev.py
@@ -1,5 +1,6 @@
-from concrete import jxTokenizer
-from dags import TrainingDataPrepPipeline
+
+from concrete import jpTokenizer
+from dags.TrainingDataPrepPipeline import TrainingDataPrepPipeline
 
 '''
 config = dict()
@@ -17,16 +18,14 @@ TrainingDataPrepPipeline.TrainingDataPrepPipeline.execute(
 '''
 
 config = dict()
-config['min_chars_for_dict_entry'] = 10
+config['min_chars_for_dict_entry'] = 1
 config['min_tokens_for_dict_entry'] = 10
-config['bpe_tokens_to_learn'] = 30000
-config['max_bpe_tokens_per_doc'] = 2048
+config['bpe_tokens_to_learn'] = 5000
+config['max_bpe_tokens_per_doc'] = 256
 config['datablock_write_trigger_size'] = 2**30
 
-TrainingDataPrepPipeline.TrainingDataPrepPipeline.execute(
-	jxTokenizer.jxTokenizer(),
-	'enhanced english comments',
-	config
-)
-
+TrainingDataPrepPipeline(
+	jpTokenizer.jpTokenizer(),
+	'jp comments dev',
+	config)
 
