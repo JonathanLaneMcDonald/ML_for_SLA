0e09851ac385045bd17d87d59386909b82a789de
Lane McDonald <Jonathan.Lane.McDonald@gmail.com>
2021-02-15 16:28:00 +1100

    clean up pipeline driver script

--- src/dev.py
+++ src/dev.py
@@ -3,29 +3,26 @@ from concrete import jpTokenizer
 from dags.TrainingDataPrepPipeline import TrainingDataPrepPipeline
 
 '''
-config = dict()
-config['min_chars_for_dict_entry'] = 10
-config['min_tokens_for_dict_entry'] = 100
-config['bpe_tokens_to_learn'] = 30000
-config['max_bpe_tokens_per_doc'] = 256
-config['datablock_write_trigger_size'] = 2**30
-
-TrainingDataPrepPipeline.TrainingDataPrepPipeline.execute(
+TrainingDataPrepPipeline(
 	jpTokenizer.jpTokenizer(),
 	'japanese comments',
-	config
-)
+	{	'min_chars_for_dict_entry': 10,
+		'min_tokens_for_dict_entry': 100,
+		'bpe_tokens_to_learn': 30000,
+		'max_bpe_tokens_per_doc': 2**14,
+		'datablock_write_trigger_size': 2**30,
+		'model_input_size': 128
+	})
 '''
-
-config = dict()
-config['min_chars_for_dict_entry'] = 1
-config['min_tokens_for_dict_entry'] = 10
-config['bpe_tokens_to_learn'] = 5000
-config['max_bpe_tokens_per_doc'] = 256
-config['datablock_write_trigger_size'] = 2**30
 
 TrainingDataPrepPipeline(
 	jpTokenizer.jpTokenizer(),
 	'jp comments dev',
-	config)
+	{	'min_chars_for_dict_entry': 1,
+		'min_tokens_for_dict_entry': 10,
+		'bpe_tokens_to_learn': 5000,
+		'max_bpe_tokens_per_doc': 2**14,
+		'datablock_write_trigger_size': 2**20,
+		'model_input_size': 128
+	})
 
