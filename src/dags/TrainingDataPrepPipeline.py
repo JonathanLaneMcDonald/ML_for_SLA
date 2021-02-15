b0d5f8c03c16182014d171fc8340b4d5806ba80f
Lane McDonald <Jonathan.Lane.McDonald@gmail.com>
2021-02-15 16:28:42 +1100

    developing and testing logic for TrainingLoop

--- src/dags/TrainingDataPrepPipeline.py
+++ src/dags/TrainingDataPrepPipeline.py
@@ -90,4 +90,22 @@ class TrainingDataPrepPipeline(ResumablePipeline):
 				config['datablock_write_trigger_size']),
 			'DatasetEncoder')
 
-		# set up some variables and then create a model checkpoint
+		# initialize a LinearDataset object and then initialize a DataGenerator object
+		token_map = {word: int(token) for word, token in
+					 [x.split() for x in open(training_dataset_token_map_path, 'r', encoding='utf-8').read().split('\n') if len(x)]}
+		dataset_files = [x for x in open(training_dataset_collection_path,'r').read().split('\n') if len(x)]
+		linear_dataset = LinearDataset(dataset_files, token_map['[SEG]'])
+		mlm_data_generator = ContextualEmbeddingsPreTrainingDataGenerator(linear_dataset, token_map, config['model_input_size'])
+		model_config = ByteNetEncoderConfig.modified_default(mlm_data_generator.get_token_count(), config['model_input_size'], 50, 200, 1, [1,1,1,1])
+		model = ByteNetEncoder.get_model(model_config)
+
+		for e in range(1000):
+			train_features, train_positions, train_labels = mlm_data_generator.generate(16384)
+			val_features, val_positions, val_labels = mlm_data_generator.generate(2048)
+			model.fit([train_features, train_positions], train_labels, batch_size=128, epochs=1,
+				verbose=1, validation_data=([val_features, val_positions], val_labels))
+
+
+
+
+
