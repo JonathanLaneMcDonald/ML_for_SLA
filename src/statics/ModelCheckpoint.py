53f5ac9bf8960035e50d62442492743d2445a824
Lane McDonald <Jonathan.Lane.McDonald@gmail.com>
2021-03-11 10:03:19 +1100

    prepping model checkpoint class for work implementing restorable objects

--- src/statics/ModelCheckpoint.py
+++ src/statics/ModelCheckpoint.py
@@ -7,7 +7,7 @@ class ModelCheckpoint:
 		This implies that the model and peripherals are all stored in a way that facilitates functionally flawless reconstruction"""
 
 	@staticmethod
-	def save_checkpoint():
+	def save_checkpoint(model_checkpoint_base_path, model, data_generator, training_schedule, evaluation_metrics=[]):
 		"""Going to need to implement get_state() type functions for all the things I want to be able to restore
 
 		I may do this in the form of an interface with a function that needs to produce a dictionary of settings
@@ -17,7 +17,7 @@ class ModelCheckpoint:
 		pass
 
 	@staticmethod
-	def load_checkpoint():
+	def load_checkpoint(checkpoint_path):
 		"""Same as above, but with a restore_state() function"""
 
 		pass
