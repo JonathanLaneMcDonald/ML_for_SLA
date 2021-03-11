43df99d9d1e05796b76e6a29c877571c8912ffa8
Lane McDonald <Jonathan.Lane.McDonald@gmail.com>
2021-03-11 11:38:20 +1100

    implemented functionality to save model checkpoints

--- src/statics/ModelCheckpoint.py
+++ src/statics/ModelCheckpoint.py
@@ -7,14 +7,31 @@ class ModelCheckpoint:
 		This implies that the model and peripherals are all stored in a way that facilitates functionally flawless reconstruction"""
 
 	@staticmethod
-	def save_checkpoint(model_checkpoint_base_path, model, data_generator, training_schedule, evaluation_metrics=[]):
+	def save_checkpoint(model_checkpoint_base_path, restorable_model, data_generator, training_schedule, evaluation_metrics=[]):
 		"""Going to need to implement get_state() type functions for all the things I want to be able to restore
 
 		I may do this in the form of an interface with a function that needs to produce a dictionary of settings
 
 		Then I'll probably just manually list all the things that need to be in the checkpoint to make a big dictionary and write a json file
 		"""
-		pass
+
+		numbered_checkpoint_path = model_checkpoint_base_path + "checkpoint " + str(training_schedule.get_current_loop())
+		latest_checkpoint_path = model_checkpoint_base_path + "latest checkpoint"
+
+		checkpoint_config = {
+			'restorable_model': restorable_model.get_state(),
+			'data_generator': data_generator.get_state(),
+			'training_schedule': training_schedule.get_state(),
+			'evaluation_metrics': [metric.get_state() for metric in evaluation_metrics]
+		}
+
+		try:
+			with open(numbered_checkpoint_path, 'w') as json_file:
+				json.dump(checkpoint_config, json_file)
+			with open(latest_checkpoint_path, 'w') as json_file:
+				json.dump(checkpoint_config, json_file)
+		except Exception as e:
+			print('Error Writing Model Checkpoints to JSON', e)
 
 	@staticmethod
 	def load_checkpoint(checkpoint_path):
