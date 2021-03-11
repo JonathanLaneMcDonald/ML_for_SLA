cd58d3a6a6c3c159141d2b84dd68329a765670f7
Lane McDonald <Jonathan.Lane.McDonald@gmail.com>
2021-03-11 11:36:25 +1100

    making the restore method static in restorable object

--- src/interfaces/AbstractRestorableObject.py
+++ src/interfaces/AbstractRestorableObject.py
@@ -5,6 +5,7 @@ class AbstractRestorableObject:
 		"""Produce a dictionary of the current state in enough detail to recreate the current state"""
 		raise Exception('AbstractRestorableObject::get_state() not implemented')
 
-	def restore_state(self, state: dict):
+	@staticmethod
+	def restore_state(state: dict):
 		"""Restore state from the provided dictionary"""
 		raise Exception('AbstractRestorableObject::restore_state() not implemented')
